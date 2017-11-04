f5e7d1f048b8786be1f71e01b9b52d3ef7222245
at4282001 <31801719+at4282001@users.noreply.github.com>
2017-11-03 23:10:35 -0700

    Add files via upload

---
+++ helperfunctions.py
@@ -0,0 +1,16 @@
+def myAdd (x,y):
+    print "Add 2 number:",x,y
+    return x+y
+
+def myDif(x,y):
+    print "Diff 2 number:",x,y
+    return x-y
+
+def myPro (x,y):
+    print "Product 2 number:",x,y
+    return x*y
+
+def myGreatest(x,y):
+    print "Greatest 2 number:",x,y
+    return max(x,y)
+
